4ca892cdd9a38a0886616b25dab7a37284d403f5
Andrei Vasil <andrei.vasil.271@gmail.com>
2022-11-14 17:36:15 +0200

    read fa from file

--- State.py
+++ State.py
@@ -2,11 +2,13 @@ class State:
     def __init__(self, token, initial=False, final=False):
         self.transitions = dict()
         self.token = token
-        self.is_final = final
-        self.is_initial = initial
+        self.final = final
+        self.initial = initial
 
     def add_transition(self, state, char):
-        self.transitions[char].append(state)
+        if state not in self.transitions:
+            self.transitions[state] = []
+        self.transitions[state].append(char)
     
     def is_DFA(self):
         for _, value in self.__transitions.items():
@@ -15,4 +17,4 @@ class State:
         return True
 
     def __str__(self):
-        return "{self.token} is_initial: {self.initial} is_final: {self.final}\n"
+        return f"{self.token} is_initial: {self.initial} is_final: {self.final}"
