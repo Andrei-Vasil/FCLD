a0f484a1904f96a14b6b7a67b0c55af98a6c20b5
917_David_Truta <72062575+917-Truta-David@users.noreply.github.com>
2022-11-28 13:50:35 +0200

    parser scheme

--- Parser.py
+++ Parser.py
@@ -1,1 +1,22 @@
-# TODO: lr(0)
+class Parser:
+    # TODO: lr(0)
+
+    def __init__(self, grammar):
+        self.grammar = grammar
+
+    def closure(self, input):
+        P = {}
+
+
+        return P
+
+    def goTo(self):
+        pass
+
+    def canonicalCollection(self):
+        res = []
+        res.add(closure(""))
+        ok = True
+
+        while ok:
+            ok = False
