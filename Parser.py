b85336366db368daeb58b1e925b526104b87dace
Andrei Vasil <andrei.vasil.271@gmail.com>
2022-12-10 20:55:46 +0200

    goto header

--- Parser.py
+++ Parser.py
@@ -1,16 +1,27 @@
-class Parser:
-    # TODO: lr(0)
+# TODO: lr(0)
 
+class Parser:
     def __init__(self, grammar):
         self.grammar = grammar
 
     def closure(self, input):
         P = {}
 
-
         return P
 
-    def goTo(self):
+    # private fun goTo(state: State, element: String): State {
+    #     val result = mutableSetOf<Item>()
+    #     for (item in state.items) {
+    #         val nonTerminal = item.rhs.getOrNull(item.dotPosition)
+    #         if (nonTerminal == element) {
+    #             val nextItem = Item(item.lhs, item.rhs, item.dotPosition + 1)
+    #             result.addAll(closure(nextItem).items)
+    #         }
+    #     }
+    #     return State(result)
+    # }
+
+    def goTo(self, state, element: str):
         pass
 
     def canonicalCollection(self):
