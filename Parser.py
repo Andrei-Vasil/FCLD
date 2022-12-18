0b7384c39825a8b98daa6bfe3ed5cb080f002c42
Andrei Vasil <andrei.vasil.271@gmail.com>
2022-12-18 14:48:24 +0200

    removed comments

--- Parser.py
+++ Parser.py
@@ -148,7 +148,6 @@ class Parser:
             table.tableRow[k[0]].goTo[k[1]] = canonicalCollection.adjacencyList[k]
         for i, state in enumerate(canonicalCollection.states):
             if state.stateType == StateType.REDUCE:
-                # TODO check this:
                 table.tableRow[i] = Row(state.stateType, None,
                                         self.orderedProductions.index(
                                             ([list(state.items.keys())[0].lhs], list(state.items.keys())[0].rhs)))
