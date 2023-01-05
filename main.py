bfeb61017e2235511e507e6e650ce4166b26b518
Andrei Vasil <andrei.vasil.271@gmail.com>
2023-01-05 19:51:06 +0200

    add examples for parser

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from Grammar import Grammar
 from Parser import Parser
 
 if __name__ == '__main__':
-    # input_file = 'input/g3.in'
+    input_file = 'input/g3.in'
     # input_file = 'input/g1.in'
-    input_file = 'input/g2.in'
+    # input_file = 'input/g2.in'
     grammar = Grammar.fromFile(input_file)
     
     # print(grammar)
@@ -25,13 +25,14 @@ if __name__ == '__main__':
 
     result = []
     try:
-        # parseTree = parser.parse(['a', 'b', 'b', 'c'])
+        parseTree = parser.parse(['a', 'b', 'b', 'c'])
+        # parseTree = parser.parse(['a', 'b', 'b', 'c', 'a'])
         # parseTree = parser.parse(['int', 'e', 'e'])
         # parseTree = parser.parse(['{', 'bool', 'identifier', ';', '}'])
         # parseTree = parser.parse(['{', 'print', '(', 'identifier', '+', 'constant', ')', ';', '}'])
         # parseTree = parser.parse(['{', 'identifier', '=', 'identifier', '+', 'constant', ';', '}'])
         # parseTree = parser.parse(['{', 'if', '(', 'identifier', '<=', 'identifier', ';', ')', '{', 'print', '(', 'constant', ')', ';', '}', '}'])
-        parseTree = parser.parse(['{', 'while', '(', 'identifier', '<=', 'identifier', ';', ')', '{', 'print', '(', 'constant', ')', ';', '}', '}'])
+        # parseTree = parser.parse(['{', 'while', '(', 'identifier', '<=', 'identifier', ';', ')', '{', 'print', '(', 'constant', ')', ';', '}', '}'])
 
         for row in parseTree:
             result.append(f'{row.index}: {row.info}, {row.parent}, {row.rightSibling}')
